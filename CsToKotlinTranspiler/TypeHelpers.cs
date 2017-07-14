97985b1e2cc82b7b63dac9cd7b9a4c4588511f59
rogeralsing <rogeralsing@gmail.com>
2017-07-14 19:58:56 +0200

    cleanup

--- CsToKotlinTranspiler/TypeHelpers.cs
+++ CsToKotlinTranspiler/TypeHelpers.cs
@@ -8,8 +8,11 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using CSharpExtensions = Microsoft.CodeAnalysis.CSharp.CSharpExtensions;
 
 namespace CsToKotlinTranspiler
 {
@@ -95,6 +98,8 @@ namespace CsToKotlinTranspiler
                 case nameof(String): return "String";
                 case nameof(TimeSpan): return "Duration";
                 case nameof(ArgumentException): return "IllegalArgumentException";
+                case nameof(ManualResetEventSlim):
+                case nameof(AutoResetEvent): return "CountDownLatch";
                 default: return name;
             }
         }
@@ -123,6 +128,7 @@ namespace CsToKotlinTranspiler
         private string GetKotlinDefaultValue(ITypeSymbol s)
         {
             var str = s.Name;
+            var named = s as INamedTypeSymbol;
             switch (str)
             {
                 case nameof(TimeSpan): return "Duration.ZERO";
@@ -131,37 +137,118 @@ namespace CsToKotlinTranspiler
                 case nameof(Double): return "0.0";
                 case nameof(Single): return "0.0";
                 case nameof(Boolean): return "false";
-                case nameof(String): return null;
-                case nameof(List<object>): return null;
-                case nameof(ISet<object>):
-                case nameof(HashSet<object>): return null;
-                case nameof(Stack<object>): return null;
-                case nameof(ConcurrentQueue<object>): return null;
-                case nameof(ConcurrentDictionary<object, object>): return null;
+                default:
+                    switch (s.TypeKind)
+                    {
+                        case TypeKind.Array: return "arrayOf()";
+                        case TypeKind.Enum:
+                            return $"{named.Name}.{named.MemberNames.First()}";
+                        case TypeKind.Struct:
+                            var t = GetKotlinType(s);
+                            return $"{t}()"; //structs are initialized to empty ctor
+                        default: return null;
+                    }
             }
-            if (s.TypeKind == TypeKind.Interface)
-            {
-                return null;
-            }
-            if (s.TypeKind == TypeKind.Array)
-            {
-                return "arrayOf()";
-            }
-            if (s is INamedTypeSymbol named)
-            {
-                if (named.TypeKind == TypeKind.Enum)
-                {
-                    return $"{named.Name}.{named.MemberNames.First()}";
-                }
+        }
 
-                if (named.TypeKind == TypeKind.Struct)
-                {
-                    var t = GetKotlinType(s);
-                    return $"{t}()"; //structs are initialized to empty ctor
-                }
-            }
+        public override void VisitInvocationExpression(InvocationExpressionSyntax node)
+        {
+            switch (node.Expression)
+            {
+                case MemberAccessExpressionSyntax member:
+                    var methodName = member.Name.Identifier.Text;
+                    var sym = CSharpExtensions.GetSymbolInfo(_model, node).Symbol;
+                    var containingTypeName = sym?.ContainingType?.Name;
 
-            return null;
+                    switch (containingTypeName)
+                    {
+                        //case nameof(AutoResetEvent):
+                        //case nameof(ManualResetEvent):
+                        //case nameof(ManualResetEventSlim):
+                        case nameof(EventWaitHandle):
+                            switch (methodName)
+                            {
+                                case nameof(AutoResetEvent.Set):
+                                    Visit(member.Expression);
+                                    Write(".countDown()");
+                                    break;
+                            }
+                            break;
+                        case nameof(Enumerable):
+                            switch (methodName)
+                            {
+                                case nameof(Enumerable.Select):
+                                    Visit(node.Expression);
+                                    Write(".");
+                                    Write("map");
+                                    Visit(node.ArgumentList);
+                                    break;
+                                case nameof(Enumerable.Where):
+                                    Visit(node.Expression);
+                                    Write(".");
+                                    Write("filter");
+                                    Visit(node.ArgumentList);
+                                    break;
+                                case nameof(Enumerable.ToList):
+                                    Visit(node.Expression);
+                                    Write(".");
+                                    Write("toList");
+                                    Visit(node.ArgumentList);
+                                    break;
+                                default:
+                                    break;
+                            }
+                            break;
+                        case nameof(Task):
+                            switch (methodName)
+                            {
+                                case nameof(Task.FromResult):
+                                    Write("");
+                                    break;
+                            }
+                            break;
+                        case nameof(Console):
+                            switch (methodName)
+                            {
+                                case nameof(Console.WriteLine):
+                                    Write("println");
+                                    Visit(node.ArgumentList);
+                                    break;
+                                case nameof(Console.Write):
+                                    Write("print");
+                                    Visit(node.ArgumentList);
+                                    break;
+                                case nameof(Console.ReadLine):
+                                    Write("readLine");
+                                    Visit(node.ArgumentList);
+                                    break;
+                            }
+                            break;
+                        default:
+                            Visit(member.Expression);
+                            Write(".");
+                            var name = member.Name.ToString();
+                            if (sym.Kind == SymbolKind.Method || sym.Kind == SymbolKind.Property)
+                            {
+                                name = ToCamelCase(name);
+                            }
+
+                            Write(name);
+                            Visit(node.ArgumentList);
+                            break;
+                    }
+                    break;
+                //  id.Call()
+                case IdentifierNameSyntax _:                
+                // .Call()
+                case MemberBindingExpressionSyntax _:      
+                    Visit(node.Expression);
+                    Visit(node.ArgumentList);
+                    break;
+                default:
+                    throw new NotSupportedException();
+            }
+           
         }
     }
 }
